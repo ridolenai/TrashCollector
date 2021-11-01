cfa6054fcb0929a933bbc372e5e37cd9a7b45247
ridolenai <92106359+ridolenai@users.noreply.github.com>
2021-11-01 13:48:51 -0500

    Employees
    Started work on the employee creation/urls/views/model code.

--- trash_collector/employees/urls.py
+++ trash_collector/employees/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-
 from . import views
 
 # TODO: Determine what distinct pages are required for the user stories, add a path for each in urlpatterns
 
 app_name = "employees"
 urlpatterns = [
-    path('', views.index, name="index")
+    path('', views.index, name="index"),
+    path('new/', views.create, name = "create"),
+    path('edit_profile/', views.edit_profile, name = "edit_profile")
 ]
--- trash_collector/employees/views.py
+++ trash_collector/employees/views.py
@@ -1,13 +1,63 @@
-from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.apps import apps
+from django.urls import reverse
+from datetime import date
+from django.core.exceptions import ObjectDoesNotExist
+from trash_collector.employees.models import Employee
+
 
 # Create your views here.
-
-# TODO: Create a function for each path created in employees/urls.py. Each will need a template as well.
-
-
+@login_required
 def index(request):
     # This line will get the Customer model from the other app, it can now be used to query the db for Customers
-    Customer = apps.get_model('customers.Customer')
-    return render(request, 'employees/index.html')
+    # logged= apps.get_model('customers.Customer')
+    # return render(request, 'employees/index.html')
+    logged_in_user = request.user
+    try:
+        logged_in_employee = Employee.objects.get(user = logged_in_user)
+        today = date.today()
+
+        context = {
+            'logged_in_employee':logged_in_employee,
+            'today':today
+        }
+    except ObjectDoesNotExist:
+        return HttpResponseRedirect(reverse('employee:create'))
+
+@login_required
+def create(request):
+    logged_in_user = request.user
+    if request.method == "POST":
+        name_from_form = request.POST.get('name')
+        address_from_form = request.POST.get('address')
+        zip_from_form = request.POST.get('zip_code')
+        weekly_from_form = request.POST.get('weekly_pickup')
+        new_employee = Employee(name=name_from_form, user=logged_in_user, address=address_from_form, zip_code=zip_from_form, weekly_pickup=weekly_from_form)
+        new_employee.save()
+        return HttpResponseRedirect(reverse('customers:index'))
+    else:
+        return render(request, 'customers/create.html')
+
+@login_required
+def edit_profile(request):
+    logged_in_user = request.user
+    logged_in_employee = Employee.objects.get(user=logged_in_user)
+    if request.method == "Post":
+        name_from_form = request.POST.get('name')
+        address_from_form = request.POST.get('address')
+        zip_from_form = request.POST.get('zip_code')
+        weekly_pickup_from_form = request.POST.get('weekly')
+        logged_in_employee.name = name_from_form
+        logged_in_employee.address = address_from_form
+        logged_in_employee.zip_code = zip_from_form
+        logged_in_employee.weekly_pickup = weekly_pickup_from_form
+        logged_in_employee.save()
+        return HttpResponseRedirect(reverse('employees:index'))
+    else:
+        context = {
+            'logged_in_employee': logged_in_employee
+        }
+        return render(request, 'employees/edit_profile.html', context)
+
